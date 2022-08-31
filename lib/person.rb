96d43b11d6888b8c053ec881823e53e3798b80ef
Sheena Sang <sheenasany@gmail.com>
2022-08-31 00:15:46 -0400

    commit tree 45426e0bc107585595d18118714aca3a33ee93ee

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
-# your code goes here
+require 'pry'
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+    
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    def happiness
+        if @happiness > 10
+            10
+       elsif @happiness < 0
+        0
+       end 
+    end
+end
+
+binding.pry
